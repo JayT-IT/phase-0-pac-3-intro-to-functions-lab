cd6197ac99b6558a00f121c563b22f8483f9507a
Trinity Lancaster <jtitschool@gmail.com>
2023-01-12 13:06:55 -0500

    Complete Lab #2

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+    
+    function whisper(string) {
+        return string.toLowerCase();
+ }
+
+ function logShout() {
+    console.log("HELLO");
+}
+logShout()
+
+function logWhisper() {
+    console.log("hello");
+}
+logWhisper();
+
+function sayHiToHeadphonedRoommate(string) {
+    `console.log('hello')`; 
+    
+    `console.log('LETS HAVE DINNER!')`;
+
+    `console.log('Let\'s have dinner together!')`;
+    
+    return `${string}`
+
+}
+sayHiToHeadphonedRoommate('I can\'t hear you!')
+
+sayHiToHeadphonedRoommate('YES INDEED!')
+
+sayHiToHeadphonedRoommate('I would love to!')
